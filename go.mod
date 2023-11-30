f438333c9c9b8994b978f85ee972cd7c8f1760e8
Rudi Farkas <rudi.farkas@gmail.com>
2023-11-30 11:25:20 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,29 @@
+module github.com/rudifa/cue-issue-2704
+
+go 1.21.0
+
+require (
+	cuelang.org/go v0.6.0
+	github.com/davecgh/go-spew v1.1.1
+	github.com/rudifa/cue-issues-fmt-comments v0.0.0-20231129163527-3b18d96a755c
+	github.com/rudifa/goutil v0.0.0-20231130100309-97ab60741014
+)
+
+require (
+	github.com/cockroachdb/apd/v3 v3.2.1 // indirect
+	github.com/emicklei/proto v1.10.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/mpvl/unique v0.0.0-20150818121801-cbe035fff7de // indirect
+	github.com/protocolbuffers/txtpbfmt v0.0.0-20230328191034-3462fbc510c0 // indirect
+	github.com/spf13/cobra v1.7.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tetratelabs/wazero v1.0.2 // indirect
+	golang.org/x/mod v0.13.0 // indirect
+	golang.org/x/net v0.16.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/tools v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	// "go/parser"
+	"log"
+
+	"cuelang.org/go/cue/parser"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/rudifa/cue-issues-fmt-comments/inproc"
+	"github.com/rudifa/goutil/files"
+)
+
+func main() {
+	// fmt.Println("Here we go")
+
+	// testParseFile("testdata/sample.cue")
+
+	// cueTestCase("testdata/sample.cue")
+
+	// sampleFile := "testdata/sample.cue"
+	// sampleFile := "testdata/2567-3a.cue"
+	// sampleFile1 := "testdata/fbb-c.cue"
+	// cueTestCase(sampleFile1)
+	// testParser(sampleFile1)
+
+	sampleFile1 := "testdata/2704.cue"
+	sampleFile2 := "testdata/2704.json"
+	cueTestCase2(sampleFile1, sampleFile2)
+
+}
+
+
+func cueTestCase2(filename1, filename2 string) {
+
+	log.Println("----------------------------------------")
+	log.Printf("cueTestCase2: [%s]\n", filename1, filename2)
+
+	files.CatFile(filename1)
+	files.CatFile(filename2)
+
+	// inproc.RunCue("version")
+
+	{
+		// copyFilename := strings.Replace(filename1, ".cue", ".copy.cue", -1)
+		// _ = files.CopyFile(filename1, copyFilename)
+
+		// inproc.RunCue("fmt", copyFilename)
+
+		// files.CatFile(copyFilename) // may be  modified
+		// files.RemoveFileIfExists(copyFilename)
+	}
+
+	inproc.RunCue("vet", filename1, filename2)
+	// inproc.RunCue("def", filename)
+
+	// inproc.RunCue("eval", filename)
+
+	// inproc.RunCue("export", filename)
+}
+
+
+func cueTestCase(filename string) {
+
+	log.Println("----------------------------------------")
+	log.Printf("cueTestCase: [%s]\n", filename)
+
+	files.CatFile(filename)
+
+	// inproc.RunCue("version")
+
+	{
+		copyFilename := strings.Replace(filename, ".cue", ".copy.cue", -1)
+		_ = files.CopyFile(filename, copyFilename)
+
+		inproc.RunCue("fmt", copyFilename)
+
+		files.CatFile(copyFilename) // may be  modified
+		files.RemoveFileIfExists(copyFilename)
+	}
+
+	// inproc.RunCue("vet", filename)
+
+	// inproc.RunCue("def", filename)
+
+	// inproc.RunCue("eval", filename)
+
+	// inproc.RunCue("export", filename)
+}
+
+func testParser(filename string) {
+
+	log.Println("test parser.ParseFile ----------------------------------------")
+	log.Printf("testParser: [%s]\n", filename)
+
+	files.CatFile(filename)
+
+	content, _ := files.ReadString(filename)
+
+	f, err := parser.ParseFile(filename, content, parser.ParseComments)
+
+	if err != nil {
+		log.Printf("unexpected error: %v\n", err)
+	}
+	// parser.LogDebugStr("spewParser", f)
+	// fmt.Println()
+
+	// fmt.Println("1 spew.Printf '%+v'")
+	// spew.Printf("f: %+v\n", f)
+
+	// fmt.Println("2 spew.Printf '%#v'")
+	// spew.Printf("f: %#v\n", f)
+
+	fmt.Println("3 spew.Dump")
+	spew.Dump(f)
+
+}
+
+// func testParseFile(filename string) {
+// 	fmt.Println("testParseFile ----------------------------------------")
+
+// 	content := files.ReadFile(filename)
+
+// 	f, err := parser.ParseFile(filename, content, parser.ParseComments)
+
+// 	if err != nil {
+// 		log.Printf("unexpected error: %v\n", err)
+// 	}
+// 	parser.LogDebugStr("test parser", f)
+
+// 	vs := fmt.Sprintf("%#v", f)
+
+// 	fmt.Println(vs)
+
+// 	spew.Dump(f)
+
+// 	spew.Printf("f: %+v\n", f)
+
+// 	spew.Printf("f: %#v\n", f)
+
+// }
